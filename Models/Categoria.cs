ee9885e0006c3d34579c46bad6fa788d4c6073ab
EduardovJS <eduardoviniciusjorge@gmail.com>
2025-02-10 19:58:14 -0300

    definindo entidades e relacionamentos

---
+++ Models/Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+
+namespace PrimeiraAPI.Models
+{
+    public class Categoria
+    {
+        public Categoria()
+        {
+            Produtos=  new Collection<Produto>();   
+        }
+
+
+        public int CategoriaId { get; set; }
+        public string? Nome { get; set; }
+        public string? ImageUrl { get; set; }
+
+        // ICollection é uma interface que representa um grupo de coisas, uma colecao generica.
+        public ICollection<Produto>? Produtos { get; set; }
+
+
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿namespace PrimeiraAPI.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        // propriedade de navegacao
+        public int CategoriaId { get; set; }
+
+
+
+    }
+}
