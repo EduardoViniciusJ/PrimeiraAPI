273d7861ea15e5b97c8923cba24b69b330cb8b66
EduardovJS <eduardoviniciusjorge@gmail.com>
2025-05-24 19:10:21 -0300

    criando os endpoints create-role, add-user-role

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -12,173 +12,218 @@ namespace PrimeiraAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-        public class AuthController : Controller
+    public class AuthController : Controller
+    {
+        private readonly ITokenService _tokenService;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(ITokenService tokenService, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
         {
-            private readonly ITokenService _tokenService;
-            private readonly UserManager<ApplicationUser> _userManager;
-            private readonly RoleManager<IdentityRole> _roleManager;
-            private readonly IConfiguration _configuration;
+            _tokenService = tokenService;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
 
-            public AuthController(ITokenService tokenService, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModelDTO loginModelDTO)
+        {
+            var user = await _userManager.FindByNameAsync(loginModelDTO.UserName!); // Encontra o usuário
+
+            // Valida o usuário e a senha fornecida
+            if (user is not null && await _userManager.CheckPasswordAsync(user, loginModelDTO.Passsword!))
             {
-                _tokenService = tokenService;
-                _userManager = userManager;
-                _roleManager = roleManager;
-                _configuration = configuration;
-            }
+                // Obtém os roles do usuário
+                var userRoles = await _userManager.GetRolesAsync(user);
 
-
-            [HttpPost("login")]
-            public async Task<IActionResult> Login([FromBody] LoginModelDTO loginModelDTO)
-            {
-                var user = await _userManager.FindByNameAsync(loginModelDTO.UserName!); // Encontra o usuário
-
-                // Valida o usuário e a senha fornecida
-                if (user is not null && await _userManager.CheckPasswordAsync(user, loginModelDTO.Passsword!))
-                {
-                    // Obtém os roles do usuário
-                    var userRoles = await _userManager.GetRolesAsync(user);
-
-                    // Cria uma lista das claims do usuário
-                    var authClaims = new List<Claim>
+                // Cria uma lista das claims do usuário
+                var authClaims = new List<Claim>
                     {
                         new Claim(ClaimTypes.Name, loginModelDTO.UserName!),
                         new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
 
                     };
 
-                    foreach (var userRole in userRoles)
-                    {
-                        authClaims.Add(new Claim(ClaimTypes.Role, userRole));  // Adiciona uma claim da lista feita para cada role do usuário.
-                    }
-
-                    // Gerando o token e passsando as claims
-                    var token = _tokenService.GenerateAccessToken(authClaims, _configuration);
-
-                    // Gerando o refresh token 
-                    var refreshToken = _tokenService.GenerateRefreshToken();
-
-                    // Obtém o tempo do refresh token e transforma para o tipo int 
-                    _ = int.TryParse(_configuration["JWT:RefreshTokenValidityInMinutes"], out int refreshTokenValidityInMinutes);
-
-
-                    user.RefreshToken = refreshToken;
-                    user.RefreshTokenExpiryTime = DateTime.Now.AddMinutes(refreshTokenValidityInMinutes);
-
-                    // Atualiza as informações do banco de dados. 
-                    await _userManager.UpdateAsync(user);
-
-                    // Retorna o token gerado, refresh token e a data de expiração.
-                    return Ok(new
-                    {
-                        Token = new JwtSecurityTokenHandler().WriteToken(token),
-                        RefreshToken = refreshToken,
-                        Expiration = token.ValidTo
-                    });
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));  // Adiciona uma claim da lista feita para cada role do usuário.
                 }
 
-                return Unauthorized();
-            }
+                // Gerando o token e passsando as claims
+                var token = _tokenService.GenerateAccessToken(authClaims, _configuration);
 
-            [HttpPost("register")]
-            public async Task<IActionResult> Register([FromBody] RegisterModelDTO registerModelDTO)
-            {
-                var userExist = await _userManager.FindByNameAsync(registerModelDTO.Username!); // Procura se o usuário existe
+                // Gerando o refresh token 
+                var refreshToken = _tokenService.GenerateRefreshToken();
 
-                if (userExist != null) // Verifica se ele existe, caso exista gera um status code 500
-                {
-                    return StatusCode(StatusCodes.Status500InternalServerError);
-
-                }
-
-                // Instancia as informações do usuário e cria um guild do security stamp
-                ApplicationUser user = new()
-                {
-                    Email = registerModelDTO.Email,
-                    SecurityStamp = Guid.NewGuid().ToString(),
-                    UserName = registerModelDTO.Username,
-                };
-
-                // Cria o usuário com sua senha
-                var result = await _userManager.CreateAsync(user, registerModelDTO.Password);
-
-                if (!result.Succeeded)
-                {
-                    return StatusCode(StatusCodes.Status500InternalServerError);
-                }
-
-                return Ok();
-            }
+                // Obtém o tempo do refresh token e transforma para o tipo int 
+                _ = int.TryParse(_configuration["JWT:RefreshTokenValidityInMinutes"], out int refreshTokenValidityInMinutes);
 
 
-            [HttpPost("refresh-token")]
-            public async Task<IActionResult> RefreshToken(TokenModelDTO tokenModelDTO)
-            {
-                if (tokenModelDTO is null)
-                {
-                    return BadRequest("Invalid client request");
-                }
+                user.RefreshToken = refreshToken;
+                user.RefreshTokenExpiryTime = DateTime.Now.AddMinutes(refreshTokenValidityInMinutes);
 
-                // Extrai os token se estiverem nulos lança uma exceção.
-                string? accessToken = tokenModelDTO.AccessToken ?? throw new ArgumentNullException(nameof(tokenModelDTO));
-                string? refreshToken = tokenModelDTO.RefreshToken ?? throw new ArgumentNullException(nameof(tokenModelDTO));
-
-                // Extrai os claims do usuário mesmo que o token de acesso esteja expirado.
-                var principal = _tokenService.GetPrincipalFromExpiredToken(accessToken, _configuration);
-
-                // Se não conseguiu extrair as claims, o token é inválido.
-                if (principal == null)
-                {
-                    return BadRequest("Invalid access token/refresh token");
-                }
-
-                // Obtém o nome do usuário com base nos claims extraídos.
-                string username = principal.Identity.Name;
-
-                // Busca no banco de dados o usuário.
-                var user = await _userManager.FindByNameAsync(username!);
-
-
-                // Valida se o usuário existe, refresh token recebido é igual o do banco de dados, o tempo do refresh token ainda não expirou
-                if (user == null || user.RefreshToken != refreshToken || user.RefreshTokenExpiryTime <= DateTime.Now)
-                {
-                    return BadRequest("Invalid access token/refresh token");
-                }
-
-                // Gera um novo token de acesso com base nas claims do token anterior
-                var newAccessToken = _tokenService.GenerateAccessToken(principal.Claims.ToList(), _configuration);
-                var newRefreshToken = _tokenService.GenerateRefreshToken();
-
-                // Atualiza o usuário com um novo refresh token no banco de dados.            
-                user.RefreshToken = newRefreshToken;
+                // Atualiza as informações do banco de dados. 
                 await _userManager.UpdateAsync(user);
 
-                // Retorna os novos tokens para o cliente
-                return new ObjectResult(new
+                // Retorna o token gerado, refresh token e a data de expiração.
+                return Ok(new
                 {
-                    accessToken = new JwtSecurityTokenHandler().WriteToken(newAccessToken),
-                    refreshToken = newRefreshToken,
+                    Token = new JwtSecurityTokenHandler().WriteToken(token),
+                    RefreshToken = refreshToken,
+                    Expiration = token.ValidTo,
+
                 });
             }
 
-            [Authorize]
-            [HttpPost("revoke/{username}")]
-            public async Task<IActionResult> Revoke(string username)
+            return Unauthorized();
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModelDTO registerModelDTO)
+        {
+            var userExist = await _userManager.FindByNameAsync(registerModelDTO.Username!); // Procura se o usuário existe
+
+            if (userExist != null) // Verifica se ele existe, caso exista gera um status code 500
             {
-                // Busca o usuário.
-                var user = await _userManager.FindByNameAsync(username);
+                return StatusCode(StatusCodes.Status500InternalServerError);
 
-                // Verifica se o usuário existe.
-                if (user == null)
-                {
-                    return BadRequest("Invalid user name");
-                }
-
-                // Faz com que o refresh token sejá nulo, forçando o usuário fazer o login.
-                user.RefreshToken = null;
-                await _userManager.UpdateAsync(user);
-
-                return NoContent();
             }
+
+            // Instancia as informações do usuário e cria um guild do security stamp
+            ApplicationUser user = new()
+            {
+                Email = registerModelDTO.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = registerModelDTO.Username,
+            };
+
+            // Cria o usuário com sua senha
+            var result = await _userManager.CreateAsync(user, registerModelDTO.Password);
+
+            if (!result.Succeeded)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return Ok(registerModelDTO);
+        }
+
+
+        [HttpPost("refresh-token")]
+        public async Task<IActionResult> RefreshToken(TokenModelDTO tokenModelDTO)
+        {
+            if (tokenModelDTO is null)
+            {
+                return BadRequest("Invalid client request");
+            }
+
+            // Extrai os token se estiverem nulos lança uma exceção.
+            string? accessToken = tokenModelDTO.AccessToken ?? throw new ArgumentNullException(nameof(tokenModelDTO));
+            string? refreshToken = tokenModelDTO.RefreshToken ?? throw new ArgumentNullException(nameof(tokenModelDTO));
+
+            // Extrai os claims do usuário mesmo que o token de acesso esteja expirado.
+            var principal = _tokenService.GetPrincipalFromExpiredToken(accessToken, _configuration);
+
+            // Se não conseguiu extrair as claims, o token é inválido.
+            if (principal == null)
+            {
+                return BadRequest("Invalid access token/refresh token");
+            }
+
+            // Obtém o nome do usuário com base nos claims extraídos.
+            string username = principal.Identity.Name;
+
+            // Busca no banco de dados o usuário.
+            var user = await _userManager.FindByNameAsync(username!);
+
+
+            // Valida se o usuário existe, refresh token recebido é igual o do banco de dados, o tempo do refresh token ainda não expirou
+            if (user == null || user.RefreshToken != refreshToken || user.RefreshTokenExpiryTime <= DateTime.Now)
+            {
+                return BadRequest("Invalid access token/refresh token");
+            }
+
+            // Gera um novo token de acesso com base nas claims do token anterior
+            var newAccessToken = _tokenService.GenerateAccessToken(principal.Claims.ToList(), _configuration);
+            var newRefreshToken = _tokenService.GenerateRefreshToken();
+
+            // Atualiza o usuário com um novo refresh token no banco de dados.            
+            user.RefreshToken = newRefreshToken;
+            await _userManager.UpdateAsync(user);
+
+            // Retorna os novos tokens para o cliente
+            return new ObjectResult(new
+            {
+                accessToken = new JwtSecurityTokenHandler().WriteToken(newAccessToken),
+                refreshToken = newRefreshToken,
+            });
+        }
+
+        [Authorize]
+        [HttpPost("revoke/{username}")]
+        public async Task<IActionResult> Revoke(string username)
+        {
+            // Busca o usuário.
+            var user = await _userManager.FindByNameAsync(username);
+
+            // Verifica se o usuário existe.
+            if (user == null)
+            {
+                return BadRequest("Invalid user name");
+            }
+
+            // Faz com que o refresh token sejá nulo, forçando o usuário fazer o login.
+            user.RefreshToken = null;
+            await _userManager.UpdateAsync(user);
+
+            return NoContent();
+        }
+
+        [HttpPost("create-role")]
+        public async Task<IActionResult> CreateRole(string roleName)
+        {
+            var roleExist = await _roleManager.RoleExistsAsync(roleName);
+
+            if (!roleExist)
+            {
+                var roleResult = await _roleManager.CreateAsync(new IdentityRole(roleName));
+
+                if (roleResult.Succeeded)
+                {
+                    return Ok($"Papel do usuário criado com sucesso {roleName}");
+                }
+                else
+                {
+                    return BadRequest($"Problemas ao criar o papel do usuário {roleName}");
+                }
+            }
+            return BadRequest($"Usuário já existe {roleName}");
+        }
+
+        [HttpPost("add-user-role")]
+        public async Task<IActionResult> AddUserToRole(string email, string roleName)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if (user != null)
+            {
+                var result = await _userManager.AddToRoleAsync(user, roleName);
+
+
+                if (result.Succeeded)
+                {
+                    return Ok($"Adicionado o papel do usuário com sucesso, usuário {email} e o papel {roleName}");
+                } else
+                {
+                    return BadRequest($"Erro ao adicionar um papel para o usuário");
+                }
+            }
+            return BadRequest("Usuário não encotrado");    
         }
     }
+
+}
+
--- obj/Debug/net8.0/PrimeiraAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/PrimeiraAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiraAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e96ff491f2e6c3f0eadb3db3364d26e068a66a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bab1b3f67adae260f4f64d71224caaa3d7769efc")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiraAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiraAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
